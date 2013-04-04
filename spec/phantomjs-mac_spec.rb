d0ba9d75e01ac83c5614855a2601006ac50f99d4
Piotr Usewicz <piotr@layer22.com>
2013-04-04 10:05:16 +0100

    Upgrade to 1.9.0
    Now the gem version matches the binary version

--- spec/phantomjs-mac_spec.rb
+++ spec/phantomjs-mac_spec.rb
@@ -1,13 +1,22 @@
-  require 'spec_helper'
+require 'spec_helper'
 
 describe Phantomjs do
   describe '.executable_path' do
     it 'works' do
-      File.exists?(Phantomjs.executable_path).should == true
+      File.exists?(Phantomjs.executable_path).should eql(true)
     end
 
     it 'points to a mac path' do
-      Phantomjs.executable_path.should match /mac/
+      Phantomjs.executable_path.should match(/mac/)
+    end
+
+    it "is executable" do
+      File.executable?(Phantomjs.executable_path).should eql(true)
+    end
+
+    it "matches the phantomjs version" do
+      version = `#{Phantomjs.executable_path} --version`.chomp
+      version.should eql(Phantomjs::Mac::VERSION)
     end
   end
 end
